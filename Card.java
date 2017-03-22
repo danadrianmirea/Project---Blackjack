34eddf1b38913c6b77e5c7bb701c834b8fa79e65
Won Yong Ha <hpmsora@gmail.com>
2017-03-22 18:31:03 -0400

    22 March 2017

---
+++ Card.java
@@ -0,0 +1,26 @@
+public class Card {
+  int cardSuit, cardNumber;
+
+  public Card(int _cardSuit, int _cardNumber) {
+    cardSuit = _cardSuit;
+    cardNumber = _cardNumber;
+  }
+
+  public int getCardSuit() {
+    return cardSuit;
+  }
+
+  public int getCardNumber() {
+    return cardNumber;
+  }
+
+  public String toString() {
+
+    String cardSuitStr, cardNumberStr;
+
+    switch (cardSuit) {
+
+    }
+    return 
+  }
+}
